d75d94be1a6646d8d17bb9752da72c1b4bcfb2a4
Anna Martin <anmartin@nmu.edu>
2023-12-04 09:21:47 -0600

    button margin

--- app.py
+++ app.py
@@ -302,7 +302,9 @@ def main():
                 image_placeholder = st.empty()
                 image_data = st.session_state['base64_image']
                 image_placeholder.markdown(f'<img src="data:image/png;base64,{image_data}" width="700" alt="Homeowner credit history image" style="display: block; margin-left: 0; margin-right: auto; margin-top: 20px; margin-bottom: 20px;">', unsafe_allow_html=True)
-                col1, col2 = st.columns(2)
+                col0, col1, col2 = st.columns([3,1])
+                with col0:
+                    st.write("")
                 with col1:
                     bad_button = st.button("Denied")
                 with col2:
