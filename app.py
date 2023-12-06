8afabc7655f76422409977f94307097859d618dc
Anna Martin <anmartin@nmu.edu>
2023-12-05 22:42:48 -0600

    added feedback box

--- app.py
+++ app.py
@@ -304,11 +304,9 @@ def main():
                 image_placeholder = st.empty()
                 image_data = st.session_state['base64_image']
                 image_placeholder.markdown(f'<img src="data:image/png;base64,{image_data}" width="700" alt="Homeowner credit history image" style="display: block; margin-left: 0; margin-right: auto; margin-top: 20px; margin-bottom: 50px;">', unsafe_allow_html=True)
-                if not st.session_state['reset_feedback_flag']:
-                    qual_feedback = st.text_input("Optional: provide feedback on your final decision. What made you agree or disagree with the model's assessment?", key="qual_feedback")
-                else:
-                    # Reset the flag after rendering the widget
-                    st.session_state['reset_feedback_flag'] = False
+
+                qual_feedback = st.text_input("Optional: provide feedback on your final decision. What made you agree or disagree with the model's assessment?", key="qual_feedback")
+
                 col0, col1, col2 = st.columns([0.5, 1, 1])
                 with col0:
                     st.write("")
@@ -372,12 +370,9 @@ def main():
                     user_query = st.text_input("Enter query here", value="", key="user_query")
 
                 submit_button = st.button(label='Submit')
-                
-                if not st.session_state['reset_feedback_flag']:
-                    qual_feedback = st.text_input("Optional: provide feedback on your final decision. What made you agree or disagree with the model's assessment?", key="qual_feedback")
-                else:
-                    # Reset the flag after rendering the widget
-                    st.session_state['reset_feedback_flag'] = False
+
+                qual_feedback = st.text_input("Optional: provide feedback on your final decision. What made you agree or disagree with the model's assessment?", key="qual_feedback")
+
                 col0, col1, col2 = st.columns([0.5, 1, 1])
                 with col0:
                     st.write("")
